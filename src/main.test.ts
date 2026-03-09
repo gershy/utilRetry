f485944d59daf71f978bb1db4c10c0c382fd1603
Gershom Maes <gershommaes@protonmail.com>
2026-03-08 21:27:34 -0400

    update deps

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,5 +1,4 @@
-import { assertEqual } from '../build/utils.test.ts';
-import util from 'node:util';
+import { assertEqual, testRunner } from '../build/utils.test.ts';
 import retry from './main.ts';
 
 // Type testing
@@ -13,126 +12,104 @@ import retry from './main.ts';
   
 })();
 
-// Test cases
-(async () => {
+testRunner([
   
-  const cases = [
-    
-    {
-      name: 'basic success',
-      fn: async () => {
-        
-        const result = await retry({
-          
-          attempts: 100,
-          delay: n => 0,
-          fn: async () => 'hello'
-          
-        });
-        
-        assertEqual(result, { val: 'hello', errs: [] });
-        
-      }
-    },
-    {
-      name: 'basic retry',
-      fn: async () => {
-        
-        await retry({
-          
-          attempts: 3,
-          delay: n => 0,
-          fn: async n => { throw Error(`ow`)[mod]({ n, retry: true }); }
-          
-        }).then(
-          
-          () => { throw Error('should have failed'); },
-          err => {
-            assertEqual(err, Error('retries exhausted')[mod]({
-              cause: Error('ow')[mod]({ n: 0, retry: true }),
-              errs: [
-                Error('ow')[mod]({ n: 0, retry: true }),
-                Error('ow')[mod]({ n: 1, retry: true }),
-                Error('ow')[mod]({ n: 2, retry: true })
-              ]
-            }))
-          }
-          
-        );
-        
-      }
-    },
-    {
-      name: 'basic prevented retry',
-      fn: async () => {
-        
-        await retry({
-          
-          attempts: 3,
-          delay: n => 0,
-          fn: async n => { throw Error(`ow`)[mod]({ n /* retry: true */ }); }
-          
-        }).then(
-          
-          () => { throw Error('should have failed'); },
-          err => {
-            assertEqual(err, Error('ow')[mod]({ n: 0 }))
-          }
-          
-        );
-        
-      }
-    },
-    {
-      name: 'retry to success',
-      fn: async () => {
-        
-        const result = await retry({
-          
-          attempts: 10,
-          delay: n => 0,
-          fn: async n => {
-            if (n !== 9) throw Error(`ow`)[mod]({ n, retry: true });
-            return 'yay'
-          }
-          
-        });
-        
-        assertEqual(result, {
-          val: 'yay',
-          errs: [
-            Error('ow')[mod]({ n: 0, retry: true }),
-            Error('ow')[mod]({ n: 1, retry: true }),
-            Error('ow')[mod]({ n: 2, retry: true }),
-            Error('ow')[mod]({ n: 3, retry: true }),
-            Error('ow')[mod]({ n: 4, retry: true }),
-            Error('ow')[mod]({ n: 5, retry: true }),
-            Error('ow')[mod]({ n: 6, retry: true }),
-            Error('ow')[mod]({ n: 7, retry: true }),
-            Error('ow')[mod]({ n: 8, retry: true }),
-          ]
-        });
-        
-      }
-    }
-    
-  ];
-  for (const { name, fn } of cases) {
-    
-    try {
+  {
+    name: 'basic success',
+    fn: async () => {
       
-      await fn();
+      const result = await retry({
+        
+        attempts: 100,
+        delay: n => 0,
+        fn: async () => 'hello'
+        
+      });
       
-    } catch (err: any) {
-      
-      console.log(`FAILED: "${name}" - error:`);
-      console.log(util.inspect(err[limn](), { depth: 8 }));
-      process.exit(1);
+      assertEqual(result, { val: 'hello', errs: [] });
       
     }
-    
+  },
+  {
+    name: 'basic retry',
+    fn: async () => {
+      
+      await retry({
+        
+        attempts: 3,
+        delay: n => 0,
+        fn: async n => { throw Error(`ow`)[mod]({ n, retry: true }); }
+        
+      }).then(
+        
+        () => { throw Error('should have failed'); },
+        err => {
+          assertEqual(err, Error('retries exhausted')[mod]({
+            cause: Error('ow')[mod]({ n: 0, retry: true }),
+            errs: [
+              Error('ow')[mod]({ n: 0, retry: true }),
+              Error('ow')[mod]({ n: 1, retry: true }),
+              Error('ow')[mod]({ n: 2, retry: true })
+            ]
+          }))
+        }
+        
+      );
+      
+    }
+  },
+  {
+    name: 'basic prevented retry',
+    fn: async () => {
+      
+      await retry({
+        
+        attempts: 3,
+        delay: n => 0,
+        fn: async n => { throw Error(`ow`)[mod]({ n /* retry: true */ }); }
+        
+      }).then(
+        
+        () => { throw Error('should have failed'); },
+        err => {
+          assertEqual(err, Error('ow')[mod]({ n: 0 }))
+        }
+        
+      );
+      
+    }
+  },
+  {
+    name: 'retry to success',
+    fn: async () => {
+      
+      const result = await retry({
+        
+        attempts: 10,
+        delay: n => 0,
+        fn: async n => {
+          if (n !== 9) throw Error(`ow`)[mod]({ n, retry: true });
+          return 'yay'
+        }
+        
+      });
+      
+      assertEqual(result, {
+        val: 'yay',
+        errs: [
+          Error('ow')[mod]({ n: 0, retry: true }),
+          Error('ow')[mod]({ n: 1, retry: true }),
+          Error('ow')[mod]({ n: 2, retry: true }),
+          Error('ow')[mod]({ n: 3, retry: true }),
+          Error('ow')[mod]({ n: 4, retry: true }),
+          Error('ow')[mod]({ n: 5, retry: true }),
+          Error('ow')[mod]({ n: 6, retry: true }),
+          Error('ow')[mod]({ n: 7, retry: true }),
+          Error('ow')[mod]({ n: 8, retry: true }),
+        ]
+      });
+      
+    }
   }
   
-  console.log(`Passed ${cases.length} test${cases.length === 1 ? '' : 's'}`);
-  
-})();
+]);
